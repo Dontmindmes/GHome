592b8841174c2414b178f0b1abfe9cf81452fd52
Gilgamesh <32211128+Dontmindmes@users.noreply.github.com>
2019-03-17 11:48:33 -0700

    Create config.go

---
+++ config.go
@@ -0,0 +1,39 @@
+{
+    "settings": {
+        "Name": "Master",
+        "Language": "en",
+        "Accent": "us"
+    },
+    "connection": {
+        "IP": "127.0.0.1",
+        "Port": 8009
+    },
+    "location": {
+        "City": "Chino",
+        "State": "CA",
+        "Country": "US",
+        "TimeZone": "America/Los_Angeles"
+    },
+    "calculation": {
+        "Method": 2
+    },
+    "prayers": {
+        "Fajir": false,
+        "Duhur": true,
+        "Asr": true,
+        "Magrib": true,
+        "Isha": true
+    },
+    "audio": {
+        "Athan": "http://127.0.0.1/Audio/Quba.mp3",
+        "Recite": "http://127.0.0.1/Audio/kahaf.mp3"
+    },
+    "volume": {
+        "Connection": false,
+        "Default": 0.55
+    },
+    "options": {
+        "Recite": true,
+        "Whisper": true
+    }
+}
